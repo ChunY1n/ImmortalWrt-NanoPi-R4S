97778a55cb0287fadd738a72766e32708009bc4e
ChunY1n <119657091+ChunY1n@users.noreply.github.com>
2026-07-02 11:18:28 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -2,8 +2,8 @@
 # 1. 修改默认管理 IP 为 10.0.0.1
 sed -i 's/192.168.1.1/10.0.0.1/g' openwrt/package/base-files/files/bin/config_generate
 
-# 2. 修改默认 Root 密码为 root
-sed -i 's/root::0:0:99999:7:::/root:$1$V479998d$wOfwMft1sl924wCwA9890.:0:0:99999:7:::/g' openwrt/package/base-files/files/etc/shadow
+# 2. 修改默认 Root 密码为 password（可自行替换成你想要的密码生成的哈希）
+sed -i 's/^root:::/root:$6$QzThC33tGP4Hx1ER$ADP8vSrbrH4CVnleMiIXgGX6\/NM9Fgxk68oOj.pXo5gEBJyWoaR0ZDTNKRRPv858Y.mj43GKpfObcopvxh8ef\/:/' openwrt/package/base-files/files/etc/shadow
 
 # 3. 分区大小仍然改根目录下的 .config（后面会被复制进 openwrt/）
 sed -i '/CONFIG_TARGET_KERNEL_PARTSIZE/d;/CONFIG_TARGET_ROOTFS_PARTSIZE/d' .config
